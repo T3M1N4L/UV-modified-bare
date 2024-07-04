95f4ae6835c36942a6238e2b6b0ed16ca82ab897
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2024-07-04 09:59:48 -0500

    main

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
+import path from "path";
+const __dirname = path.resolve();
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: `${libcurlPath}/**/*`.replace(/\\/g, "/"),
+          dest: "libcurl",
+          overwrite: false
+        },
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/w/": {
+        target: "http://localhost:4000/",
+        rewrite: (path) => path.replace(/^\/w/, ''),
+        ws: true
+      },
+    }
+  }
+})
