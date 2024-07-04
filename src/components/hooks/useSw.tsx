bf37a449c120d8b23b125109c2f22b92fb89b4d4
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2024-07-04 10:12:16 -0500

    bruh

---
+++ src/components/hooks/useSw.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+// @ts-expect-error no types
+import { SetTransport } from "@mercuryworkshop/bare-mux";
+
+const useSw = (path: string) => {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.ready.then(() => {
+        SetTransport("CurlMod.LibcurlClient", {
+          wisp: `${location.port == "443" ? "wss://" : "ws://"}${
+            location.host
+          }/w/`
+        });
+      })
+      navigator.serviceWorker
+      .register(path)
+        .then(
+          function (registration) {
+            console.log(
+              `[sw] ${path} successfuly registered with a scope of ${registration.scope}`
+            );
+          },
+          function (err) {
+            console.log(
+              `%c[sw] ${path} failed to register, error: `,
+              "color:red;",
+              err
+            );
+          }
+        );
+    }
+  }, [path]);
+};
+
+export default useSw;
