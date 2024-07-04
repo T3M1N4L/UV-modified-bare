942bd98afdc1ce6c86aa144777d33214377f0641
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2024-07-04 10:07:58 -0500

    meh

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+
+importScripts("/uv/uv.bundle.js");
+importScripts("/uv/uv.sw.js");
+importScripts("/uv/uv.config.js");
+importScripts("/libcurl/index.js");
+
+// Credit to MotorTruck1221 <@818995901791207454> for the bare switcher code
+
+uv = new UVServiceWorker();
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+      (async ()=>{
+          if(event.request.url.startsWith(location.origin + __uv$config.prefix)) {
+              return await uv.fetch(event);
+          }
+          return await fetch(event.request);
+      })()
+    );
+  });
